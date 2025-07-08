024a66f489cc000d25688d081d4c48593e6e524f
Nancia Laudino <nancialaudino@gmail.com>
2025-07-08 11:11:08 +0200

    Add URLs, views and controllers

--- pokemon-project/routes/web.php
+++ pokemon-project/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomepageController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomepageController::class, 'index']);
+Route::get('/product', [ProductController::class, 'index']);
+Route::get('/user', [UserController::class, 'showUser']);
